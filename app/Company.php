5f1bf894c689d223dad4a5b4820191d983e8b2dc
MasanaIto <z3heng0326@gmail.com>
2020-03-15 22:43:19 +0900

    Companyモデルを作成

--- app/Company.php
+++ app/Company.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class Company extends Model
 {
-    //
+    public function getList() {
+        $fstatement_list = DB::table('financial_statements')
+                      ->select('financial_statements.*')
+                      ->orderBy('financial_statements.company_id', 'DESC');
+        return $fstatement_list;
+    }
 }
