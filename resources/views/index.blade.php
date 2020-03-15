3670bc6820180c9387f6c220c28aae8f4e17de2c
MasanaIto <z3heng0326@gmail.com>
2020-03-15 10:43:20 +0900

    home画面を作成、ルート定義を追加

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Finchart</title>
+    </head>
+    <body>
+        @extends('layouts.base')
+
+        @section('content')
+            <h1>トップページ</h1>
+        @endsection
+    </body>
+</html>
