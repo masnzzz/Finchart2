cb9703a01aa1f846b134684651526a3b81eb6239
MasanaIto <z3heng0326@gmail.com>
2020-03-15 22:44:08 +0900

    トップページの表を作成 jQuery導入

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,36 @@
         @extends('layouts.base')
 
         @section('content')
+        <script>
+            jQuery(function($){
+                $.extend( $.fn.dataTable.defaults, {
+                    language: {
+                        url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
+                        }
+                        });
+                $("#fstatement_list").DataTable({
+                    "searching": true,     //検索ボックスでのテキスト検索を可能にする
+                    "paging":   true,      //ページングを可能にする
+                    "ordering": true,      //各項目でのソートを可能にする
+                    "lengthChange": true,  //件数の切り替え機能を可能にする
+                    
+                    }).columns.adjust().draw();
+                });
+        </script>
             <h1>トップページ</h1>
+
+            <table id="fstatement_list" width="100%" class="table table-striped table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th class="text-center" width="40%">会社</th>
+                    <th class="text-center" width="40%">決算日</th>
+                    <th class="text-center">LINK</th>
+                </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
         @endsection
     </body>
 </html>
