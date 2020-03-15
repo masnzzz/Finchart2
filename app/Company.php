17d905a46e844ba3eb1e31ec7246040dd8756f4d
MasanaIto <z3heng0326@gmail.com>
2020-03-16 00:50:46 +0900

    Companyモデルを作成
    Companyモデルにはcompaniesテーブル、financial_statementsテーブルを持たせる

--- app/Company.php
+++ app/Company.php
@@ -2,13 +2,20 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class Company extends Model
 {
-    public function getList() {
+    public function companies()
+    {
+        return $this->hasMany('App\Company');
+    }
+
+    public function getList()
+    {
         $fstatement_list = DB::table('financial_statements')
                       ->select('financial_statements.*')
                       ->orderBy('financial_statements.company_id', 'DESC');
